7bc13bada69c39928c226a5454324c56fcdd2659
deiliet <thedeiliet@gmail.com>
2020-02-02 13:41:54 +0300

    Добавил разделители 2

--- 06_control_structures/task_6_2a.py
+++ 06_control_structures/task_6_2a.py
@@ -34,14 +34,14 @@ else:
         elif (octet_list[0] >= 224) and (octet_list[0] <= 239):
             print(f"\033[93m{'multicast'}")
         elif (octet_list[0] == 255) and \
-                (octet_list[1] == 255) and \
-                (octet_list[2] == 255) and \
-                (octet_list[3] == 255):
+                octet_list[1] == 255 and \
+                octet_list[2] == 255 and \
+                octet_list[3] == 255:
             print(f"\033[93m{'local broadcast'}")
         elif (octet_list[0] == 0) and \
-                (octet_list[1] == 0) and \
-                (octet_list[2] == 0) and \
-                (octet_list[3] == 0):
+                octet_list[1] == 0 and \
+                octet_list[2] == 0 and \
+                octet_list[3] == 0:
             print(f"\033[93m{'unassigned'}")
         else:
             print(f"\033[93m{'unused'}")
