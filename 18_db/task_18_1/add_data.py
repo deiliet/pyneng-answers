6ec1b369f209fa17c436380dfd29e57fea35c208
deiliet <thedeiliet@gmail.com>
2020-02-17 13:55:45 +0300

    Complete task 2

--- 18_db/task_18_1/add_data.py
+++ 18_db/task_18_1/add_data.py
@@ -4,6 +4,32 @@ from glob import glob
 import sqlite3
 
 
+def add_to_switches(switches_list, switch_table, connection):
+    print('Adding data in table "swtiches"')
+    for item in switches_list:
+        try:
+            sql_command_switches = f"INSERT INTO {switch_table} VALUES {item};"
+            connection.execute(sql_command_switches)
+        except sqlite3.IntegrityError:
+            print(f'While adding data: {item} Error is occurred: UNIQUE constraint failed: switches.hostname')
+
+
+def add_to_dhcp(dhcp_file_list, regexp_device, regexp_dhcp, dhcp_table, connection):
+    print('Adding data in table "dhcp"')
+    for dhcp_file in dhcp_file_list:
+        with open(dhcp_file) as file:
+            device_name = re.match(regexp_device, dhcp_file).group()
+            # dhcp_value_tuple = [(*item.groups(), device_name) for item in re.finditer(regexp_dhcp, file.read(), re.DOTALL)]
+            dhcp_value_tuple = [item.groups() + (device_name,) for item in
+                                re.finditer(regexp_dhcp, file.read(), re.DOTALL)]
+            for item in dhcp_value_tuple:
+                try:
+                    sql_command_dhcp = f"INSERT INTO {dhcp_table} VALUES {item};"
+                    connection.execute(sql_command_dhcp)
+                except sqlite3.IntegrityError:
+                    print(f'While adding data: {item} Error is occurred: UNIQUE constraint failed: dhcp.mac')
+
+
 def add_db_data(yaml_filename, dhcp_file_list, db_filename, switch_table, dhcp_table):
 
     regexp_dhcp = r"(?P<mac_address>(?:[\dABCDEF]+:){5}[\dABCDEF]+) +" \
@@ -22,28 +48,11 @@ def add_db_data(yaml_filename, dhcp_file_list, db_filename, switch_table, dhcp_t
         switches_list = list(switch_info_dict['switches'].items())
         connection = sqlite3.connect(db_filename)
         with connection:
-            print('Adding data in table "swtiches"')
-            for item in switches_list:
-                try:
-                    sql_command_switches = f"INSERT INTO {switch_table} VALUES {item};"
-                    connection.execute(sql_command_switches)
-                except sqlite3.IntegrityError:
-                    print(f'While adding data: {item} Error is occurred: UNIQUE constraint failed: switches.hostname')
-            print('Adding data in table "dhcp"')
-            for dhcp_file in dhcp_file_list:
-                with open(dhcp_file) as file:
-                    device_name = re.match(regexp_device, dhcp_file).group()
-                    # dhcp_value_tuple = [(*item.groups(), device_name) for item in re.finditer(regexp_dhcp, file.read(), re.DOTALL)]
-                    dhcp_value_tuple = [item.groups() + (device_name,) for item in re.finditer(regexp_dhcp, file.read(), re.DOTALL)]
-                    for item in dhcp_value_tuple:
-                        try:
-                            sql_command_dhcp = f"INSERT INTO {dhcp_table} VALUES {item};"
-                            connection.execute(sql_command_dhcp)
-                        except sqlite3.IntegrityError:
-                            print(f'While adding data: {item} Error is occurred: UNIQUE constraint failed: dhcp.mac')
+            add_to_switches(switches_list, switch_table, connection)
+            add_to_dhcp(dhcp_file_list, regexp_device, regexp_dhcp, dhcp_table, connection)
     return None
 
 
 if __name__ == "__main__":
-    dhcp_file_list = sorted(glob('*dhcp_snooping.txt'))
-    add_db_data('switches.yml', dhcp_file_list, 'dhcp_snooping.db', 'switches', 'dhcp')
+    dhcp_file_list_glob = sorted(glob('*dhcp_snooping.txt'))
+    add_db_data('switches.yml', dhcp_file_list_glob, 'dhcp_snooping.db', 'switches', 'dhcp')
--- 18_db/task_18_1/create_db.py
+++ 18_db/task_18_1/create_db.py
@@ -1,18 +1,10 @@
-
 import sys
 import re
 import glob
 import sqlite3
 
 
-def db_file_create(db_filename='default.db', sql_create_filename='default.sql'):
-
-    db_file_list = sorted(glob.glob(db_filename))
-    sql_create_file_list = sorted(glob.glob(sql_create_filename))
-    regexp_db = r'\S+(\.db)'
-    regexp_sql = r'\S+(\.sql)'
-    regexp_sql_create = r'create table (?P<table_name>.*?) \(.*?\);'
-
+def db_file_checker(db_file_list, db_filename, regexp_db):
     if not db_file_list and 'default.db' in db_filename:
         print('DataBase File is not existed. Empty Default DataBase File is created')
     elif not db_file_list:
@@ -31,10 +23,9 @@ def db_file_create(db_filename='default.db', sql_create_filename='default.sql'):
         print('File is not DataBase or whitespaces are existed')
         sys.exit('Change DataBase filename')
 
-    connection = sqlite3.connect(db_filename)
-    cursor = connection.cursor()
 
-    if not sql_create_file_list and 'default.db' in db_filename:
+def sql_create_file_checker(sql_create_file_list, sql_create_filename, regexp_sql, regexp_sql_create):
+    if not sql_create_file_list and 'default.sql' in sql_create_filename:
         default_sql_create_file = open(sql_create_filename, 'w')
         default_sql_create_file.close()
         print('SQL-CreateSctipt File is not existed. Empty Default SQL-CreateSctipt File is created')
@@ -56,6 +47,8 @@ def db_file_create(db_filename='default.db', sql_create_filename='default.sql'):
         print('File is not SQL-CreateSctipt or whitespaces are existed')
         sys.exit('Change SQL-CreateSctipt filename')
 
+
+def execute_sql_create (sql_create_filename, regexp_sql_create, cursor):
     with open(sql_create_filename, 'r') as file:
         for match_index in re.finditer(regexp_sql_create, file.read(), re.DOTALL):
             print(match_index.group())
@@ -65,6 +58,23 @@ def db_file_create(db_filename='default.db', sql_create_filename='default.sql'):
                     print(f'Table "{match_index.group("table_name")}" is sucsessfully created')
                 except sqlite3.OperationalError:
                     print(f'Table "{match_index.group("table_name")}" in DataBase is already existed')
+
+
+def db_file_create(db_filename='default.db', sql_create_filename='default.sql'):
+
+    db_file_list = sorted(glob.glob(db_filename))
+    sql_create_file_list = sorted(glob.glob(sql_create_filename))
+    regexp_db = r'\S+(\.db)'
+    regexp_sql = r'\S+(\.sql)'
+    regexp_sql_create = r'create table (?P<table_name>.*?) \(.*?\);'
+
+    db_file_checker(db_file_list, db_filename, regexp_db)
+    sql_create_file_checker(sql_create_file_list, sql_create_filename, regexp_sql, regexp_sql_create)
+
+    connection = sqlite3.connect(db_filename)
+    cursor = connection.cursor()
+
+    execute_sql_create(sql_create_filename, regexp_sql_create, cursor)
 
     return None
 
